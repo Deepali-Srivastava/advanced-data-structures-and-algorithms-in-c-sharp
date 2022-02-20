b25a94b746765ca706e89cd16f4461e1275f513b
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-20 17:12:54 +0530

    Path matrix in C#

---
+++ graph/PathMatrixProject/Demo.cs
@@ -0,0 +1,47 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace PathMatrixProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+          DirectedGraph g1 = new DirectedGraph();
+		        
+		  g1.InsertVertex("Zero");
+  		  g1.InsertVertex("One");
+		  g1.InsertVertex("Two");
+		  g1.InsertVertex("Three");
+
+		  g1.InsertEdge("Zero","One");
+		  g1.InsertEdge("Zero","Three");
+		  g1.InsertEdge("One","Two");
+		  g1.InsertEdge("One","Three");
+		  g1.InsertEdge("Three","Two");
+		  g1.FindPathMatrix();
+		  
+		  Console.WriteLine();
+		 
+		  DirectedGraph g2 = new DirectedGraph();
+		  
+		  g2.InsertVertex("Zero");
+  		  g2.InsertVertex("One");
+		  g2.InsertVertex("Two");
+		  g2.InsertVertex("Three");
+		  
+		  g2.InsertEdge("Zero","One");
+		  g2.InsertEdge("Zero","Three");
+		  g2.InsertEdge("One","Two");
+		  g2.InsertEdge("One","Three");
+		  g2.InsertEdge("Two","Zero");
+		  g2.InsertEdge("Three","Two");
+		  g2.FindPathMatrix();
+       	}	
+    }
+}
+
---
+++ graph/PathMatrixProject/DirectedGraph.cs
@@ -0,0 +1,126 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace PathMatrixProject
+{
+    class DirectedGraph
+    {
+        public readonly int MAX_VERTICES = 30;
+
+	    int n;           
+	    int e;           
+	    bool [,] adj; 
+	    Vertex [] vertexList;
+         
+        public DirectedGraph()
+        {
+	        adj = new bool[MAX_VERTICES,MAX_VERTICES];
+	        vertexList = new Vertex[MAX_VERTICES];
+        }
+   
+        public void FindPathMatrix()
+        {	             
+            int [,] x = new int[n,n];
+            int[,] adjp = new int[n, n];
+            int[,] temp = new int[n, n];
+			   
+	        for (int i = 0; i<n; i++)
+		        for (int j = 0; j<n; j++)
+			        x[i,j] = adjp[i,j] = adj[i,j] ? 1 : 0 ;
+	   
+	        for (int p = 2; p <= n; p++)
+	        {
+		        for (int i = 0; i<n; i++)
+			        for (int j = 0; j<n; j++)
+			        {
+					    temp[i,j] = 0;
+					    for (int k = 0; k<n; k++)
+						    temp[i,j] = temp[i,j] + adjp[i,k] * (adj[k,j] ? 1 : 0);
+			        }
+		   	
+		        for (int i = 0; i<n; i++)
+			        for (int j = 0; j<n; j++)
+				        adjp[i,j] = temp[i,j];
+   		   
+		        for (int i = 0; i<n; i++)
+			        for (int j = 0; j<n; j++)
+		  		        x[i,j] = x[i,j] + adjp[i,j];
+	        }
+	   
+	        //Display X
+	        for (int i = 0; i<n; i++)
+	        {
+	            for (int j = 0; j<n; j++)
+	                Console.Write(x[i,j] + " ");
+	            Console.WriteLine();
+	        }
+	        Console.WriteLine();      
+	  
+	        bool [,] path = new bool[n,n];
+	        
+            for (int i = 0; i<n; i++)
+		        for (int j = 0; j<n; j++)
+			   	    if (x[i,j] == 0 )
+					    path[i,j] = false;
+				    else
+					    path[i,j] = true;	   	
+		   
+	        //Display Path Matrix
+	        for (int i = 0; i<n; i++)
+	        {
+	            for (int j = 0; j<n; j++)
+	                if (path[i,j]) 
+	                    Console.Write("1 ");
+	                else
+	                    Console.Write("0 ");
+	                Console.WriteLine();
+	        }
+	    }
+
+        public void Display()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    if (adj[i, j])
+                        Console.Write("1 ");
+                    else
+                        Console.Write("0 ");
+                Console.WriteLine();
+            }
+        }
+
+        public void InsertVertex(String name)
+        {
+            vertexList[n++] = new Vertex(name);
+        }
+
+        private int GetIndex(String s)
+        {
+            for (int i = 0; i < n; i++)
+                if (s.Equals(vertexList[i].name))
+                    return i;
+            throw new System.InvalidOperationException("Invalid Vertex");
+        }
+
+        /*Insert an edge (s1,s2) */
+        public void InsertEdge(String s1, String s2)
+        {
+            int u = GetIndex(s1);
+            int v = GetIndex(s2);
+            if (u == v)
+                throw new System.InvalidOperationException("Not a valid edge");
+            if (adj[u, v] == true)
+                Console.Write("Edge already present");
+            else
+            {
+                adj[u, v] = true;
+                e++;
+            }
+        }
+    }
+}
